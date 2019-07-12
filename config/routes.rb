a05caaceaee418d67b2aac4a83e1bad75e65808c
松岡亮佑 <matsuokaryousuke@matsuokaryousukenoMacBook-Pro.local>
2019-07-12 18:56:07 +0900

    issue#23

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/admin/users/:id/favorite', to: 'users#like'
 
   namespace :admin do
     resources :users
