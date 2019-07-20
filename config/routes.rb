917e70c0661ac95e8a3d7e59d857035a7140f8c4
松岡亮佑 <matsuokaryousuke@matsuokukenoMBP.lan>
2019-07-20 22:08:20 +0900

    add_category_resource

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
     resources :comments
 
   end
-  get '/categories/:id', to:'categories#show'
+  resources :categories
+  #get '/categories/:id', to:'categories#show'
 end
