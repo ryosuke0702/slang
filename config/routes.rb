447f2a08039276d5059540200385b323995bedbf
ryosuke0702 <51069529+ryosuke0702@users.noreply.github.com>
2019-08-08 00:51:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     #match "/auth/:provider/callback" => 'sessions#createByFacebook', via: 'get'
 
     get '/login', to: 'sessions#new'
-    post '/login', to: 'sessions#create'
+    #post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
 
     #get '/auth/:provider/callback', to: 'sessions#create'
