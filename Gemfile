e14b59543e1ee3b99a4581611d5c22d1c7be4a35
松岡亮佑 <matsuokaryousuke@matsuokaryousukenoMacBook-Pro.local>
2019-09-14 22:51:46 +0900

    [fix]google-login

--- Gemfile
+++ Gemfile
@@ -83,3 +83,4 @@ gem 'omniauth-facebook'
 gem 'omniauth-google-oauth2'
 
 gem 'dotenv-rails'
+gem "figaro"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,ENV['FACEBOOK_KEY'],ENV['FACEBOOK_SECRET']
-  provider :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET'],{:skip_jwt =>true}
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :facebook,ENV['FACEBOOK_KEY'],ENV['FACEBOOK_SECRET']
+#  provider :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET'],{:skip_jwt =>true}
 
   #{:provider_ignores_state => true}
+#end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret
 end
