23f63e75f22a599921bfb12b8726a92f10534f40
松岡亮佑 <matsuokaryousuke@matsuokaryousukenoMacBook-Pro.local>
2019-09-14 22:22:42 +0900

    [fix]google-login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,ENV['FACEBOOK_KEY'],ENV['FACEBOOK_SECRET']
   provider :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET'],{:skip_jwt =>true}
 
-  {:provider_ignores_state => true}
+  #{:provider_ignores_state => true}
 end
